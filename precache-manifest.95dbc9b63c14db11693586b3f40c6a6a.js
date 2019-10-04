ecda7be45a7a2d17e93263f5ecf93b1d032b2ee2
openopen <openopen114@gmail.com>
2019-10-04 19:34:16 +0800

    Updates

---
+++ precache-manifest.95dbc9b63c14db11693586b3f40c6a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91de9248cb992e22bc10670f44e4fe1a",
+    "url": "/hooks-use-reducer-demo/index.html"
+  },
+  {
+    "revision": "00943812343b242f773d",
+    "url": "/hooks-use-reducer-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "645e5e08884ace14ccc4",
+    "url": "/hooks-use-reducer-demo/static/js/2.956c2ce5.chunk.js"
+  },
+  {
+    "revision": "00943812343b242f773d",
+    "url": "/hooks-use-reducer-demo/static/js/main.0ac37057.chunk.js"
+  },
+  {
+    "revision": "2e16fedad79280bf4c32",
+    "url": "/hooks-use-reducer-demo/static/js/runtime-main.305a8c2a.js"
+  }
+]);
